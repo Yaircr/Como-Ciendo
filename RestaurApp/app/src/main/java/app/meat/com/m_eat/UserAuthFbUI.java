73adc98b92da112461391809bb3bc03f70c2c1a7
Yair Cuellar Rivera <edyhpdouble@gmail.com>
2017-07-21 22:32:33 -0500

    Started working on FIrebaseUI implementation

---
+++ RestaurApp/app/src/main/java/app/meat/com/m_eat/UserAuthFbUI.java
@@ -0,0 +1,23 @@
+package app.meat.com.m_eat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by EduardoYair on 21/07/2017.
+ */
+
+public class UserAuthFbUI extends AppCompatActivity{
+
+    // private AppCompatActivity mBaseActivity; // used in fragment to store parent
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //mBaseActivity = (AppCompatActivity) getActivity();
+
+
+    }
+}
